16a7e2d10ddeb58a72da68e1cb2bd4a1d742a0ca
unicodesnowman <utfsnowman@gmail.com>
2017-02-01 00:02:11 -0500

    up to 66

--- src/offline_4clojure/p66.clj
+++ src/offline_4clojure/p66.clj
@@ -6,14 +6,11 @@
 (ns offline-4clojure.p66
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(defn gcd [a b] false)
 
 (defn -main []
   (are [soln] soln
-(= (__ 2 4) 2)
-(= (__ 10 5) 5)
-(= (__ 5 7) 1)
-(= (__ 1023 858) 33)
-))
+       (= (gcd 2 4) 2)
+       (= (gcd 10 5) 5)
+       (= (gcd 5 7) 1)
+       (= (gcd 1023 858) 33)))
