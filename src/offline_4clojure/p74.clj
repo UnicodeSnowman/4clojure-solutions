d74aa9f1e60416e3a3f98686a4ce8b41c3cf7a3a
unicodesnowman <utfsnowman@gmail.com>
2017-02-14 07:43:41 -0500

    starting on 75
    * my gcd fn from p66 seems broken... why?

--- src/offline_4clojure/p74.clj
+++ src/offline_4clojure/p74.clj
@@ -1,16 +1,24 @@
 ;; Filter Perfect Squares - Medium
-;; Given a string of comma separated integers, write a function which returns a new comma separated string that only contains the numbers which are perfect squares.
+;; Given a string of comma separated integers, write a function which returns a new comma separated string
+;; that only contains the numbers which are perfect squares.
 ;; tags - 
 ;; restricted - 
 (ns offline-4clojure.p74
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+; NOTE could also use
+; (= (Math/sqrt n) (Math/floor (Math/sqrt n)))
+; to check for perfect square
+(defn perfect-square? [n]
+  (zero? (mod (Math/sqrt n) 1.0)))
+
+(defn perfect-squares [s]
+  (->> (clojure.string/split s #",")
+       (mapv #(Integer/parseInt %))
+       (filter perfect-square?)
+       (clojure.string/join ",")))
 
 (defn -main []
   (are [soln] soln
-(= (__ "4,5,6,7,8,9") "4,9")
-(= (__ "15,16,25,36,37") "16,25,36")
-))
+       (= (perfect-squares "4,5,6,7,8,9") "4,9")
+       (= (perfect-squares "15,16,25,36,37") "16,25,36")))
--- src/offline_4clojure/p75.clj
+++ src/offline_4clojure/p75.clj
@@ -1,18 +1,31 @@
 ;; Euler's Totient Function - Medium
-;; Two numbers are coprime if their greatest common divisor equals 1.  Euler's totient function f(x) is defined as the number of positive integers less than x which are coprime to x.  The special case f(1) equals 1.  Write a function which calculates Euler's totient function.
+;; Two numbers are coprime if their greatest common divisor equals 1.
+;; Euler's totient function f(x) is defined as the number of positive integers less than x which are
+;; coprime to x.
+;; The special case f(1) equals 1.  Write a function which calculates Euler's totient function.
 ;; tags - 
 ;; restricted - 
 (ns offline-4clojure.p75
+  (:require [offline-4clojure.p66 :refer [gcd]])
   (:use clojure.test))
 
-(def __
-;; your solution here
-)
+(defn coprime? [a b]
+  (= 1 (gcd a b)))
+
+(defn totient [x]
+  (filter (partial coprime? x) (range 1 (inc x))))
+
+; apparently my gcd function is incorrect? need to think more on
+; why...
+(defn totient [n]
+  (if (= n 1)
+    1
+    (let [gcd (fn [a b] (if (zero? b) a (recur b (mod a b))))]
+      (count (filter #{1} (map (partial gcd n) (range 1 n)))))))
 
 (defn -main []
   (are [soln] soln
-(= (__ 1) 1)
-(= (__ 10) (count '(1 3 7 9)) 4)
-(= (__ 40) 16)
-(= (__ 99) 60)
-))
+       (= (totient 1) 1)
+       (= (totient 10) (count '(1 3 7 9)) 4)
+       (= (totient 40) 16)
+       (= (totient 99) 60)))
