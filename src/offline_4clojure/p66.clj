e21321c64f9817c79183c78f306a7a7ed76c5693
unicodesnowman <utfsnowman@gmail.com>
2017-02-18 15:44:39 -0500

    fix gcd

--- src/offline_4clojure/p66.clj
+++ src/offline_4clojure/p66.clj
@@ -7,12 +7,9 @@
   (:use clojure.test))
 
 (defn gcd [a b]
-  (let [max-value (max a b)
-        min-value (min a b)
-        test-val (mod max-value min-value)]
-    (if (zero? test-val)
-      min-value
-      (recur max-value test-val))))
+  (if (zero? b)
+    a
+    (recur b (mod a b))))
 
 (defn -main []
   (are [soln] soln
@@ -20,3 +17,19 @@
        (= (gcd 10 5) 5)
        (= (gcd 5 7) 1)
        (= (gcd 1023 858) 33)))
+
+; works for any order of operations... need to check for `zero?` value
+; of whatever number is in the denominator of the modulo. i.e.
+; this could also be
+
+(defn alt-gcd [a b]
+  (if (zero? a)
+    b
+    (recur (mod b a) a)))
+
+; (gcd 10 5)
+; 5 0 => 5
+; 
+; (gcd 5 10)
+; 10 5
+; 5 0 => 5
