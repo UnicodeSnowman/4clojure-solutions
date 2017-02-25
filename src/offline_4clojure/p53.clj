8f168157fb6c542b1a6ed4ebfdc9ce655d5a425e
unicodesnowman <utfsnowman@gmail.com>
2017-02-25 17:54:14 -0500

    FINALLY getting around to finishing 53

--- src/offline_4clojure/p53.clj
+++ src/offline_4clojure/p53.clj
@@ -1,16 +1,26 @@
 ;; Longest Increasing Sub-Seq - Hard
-;; Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers. If two sub-sequences have the same length, use the one that occurs first. An increasing sub-sequence must have a length of 2 or greater to qualify.
+;; Given a vector of integers, find the longest consecutive sub-sequence of increasing numbers.
+;; If two sub-sequences have the same length, use the one that occurs first.
+;; An increasing sub-sequence must have a length of 2 or greater to qualify.
 ;; tags - seqs
 ;; restricted - 
 (ns offline-4clojure.p53
   (:use clojure.test))
 
 (defn valid? [coll]
-  (apply < coll))
+  (and
+    (>= (count coll) 2)
+    (= coll (range (first coll) (inc (last coll))))))
 
-(defn longest-increasing-sub-seq [xs]
-  (while (seq xs)
-    ))
+(defn longest-increasing-sub-seq
+  ([xs] (longest-increasing-sub-seq xs (count xs)))
+  ([xs n]
+   (if (< n 2)
+     []
+     (let [valid-seqs (filter valid? (partition n 1 xs))]
+       (if (empty? valid-seqs)
+         (recur xs (dec n))
+         (first valid-seqs))))))
 
 (defn -main []
   (are [soln] soln
@@ -18,16 +28,3 @@
        (= (longest-increasing-sub-seq [5 6 1 3 2 7]) [5 6])
        (= (longest-increasing-sub-seq [2 3 3 4 5]) [3 4 5])
        (= (longest-increasing-sub-seq [7 6 5 4]) [])))
-
-(def blah [1 0 1 2 3 0 4 5])
-(def blah [5 6 1 3 2 7])
-(def blah [2 3 3 4 5])
-
-(partition-by < blah)
-(split-with < blah)
-
-(partition 2 1 blah)
-
-(map (fn [a b]
-       [b a]) blah (cons nil (butlast blah)))
-
